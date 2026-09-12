a618df0e425a1aee256a8b934d91912a9dc17efe
surajpal01 <suraj88876124@gmail.com>
2026-09-12 11:41:26 +0530

    Add files via upload

---
+++ Task3_deletion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    for(i = pos - 1; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+
+    n--;
+
+    printf("Array after deletion:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
